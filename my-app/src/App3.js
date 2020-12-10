e81979f7c56b3f788d4801c5313e62dd5062ebaf
raisgood <rashadthecoder@outlook.com>
2020-12-10 11:17:24 -0600

    update

--- my-app/src/App3.js
+++ my-app/src/App3.js
@@ -1,76 +1,49 @@
-import './App.css';
+/*import './App.css';
 import './App2';
 
 function App3() {
-    return ( <
-        div className = "App3" >
-        <
-        header >
-        <
-        div id = "myDIV"
-        className = "header" >
-        <
-        h2 > My To Do List < /h2> <
-        /div> <
-        /header> <
-        section >
-        <
-        ul id = "myUL" >
-        <
-        li > < input type = "checkbox" / > Hit the gym < /li> <
-        li > Pay bills < input type = "checkbox" / > < /li> <
-        li > Meet Charlotte < input type = "checkbox" / > < /li> <
-        li > Buy Groceries < input type = "checkbox" / > < /li> <
-        li > Study < input type = "checkbox" / > < /li> <
-        li > Hit the Sack < input type = "checkbox" / > < /li> <
-        /ul> <
-        /section> <
-        section >
-        <
-        h1 id = "contactHeading" > HOW CAN WE DO BETTER ? < /h1> <
-        form action = "https://formspree.io/xqkyryyz"
-        method = "POST" >
-        <
-        label >
-        <
-        br / >
-        Name / Company :
-        <
-        br / >
-        <
-        input class = "inputBox"
-        type = "text"
-        name = "name" / >
-        <
-        /label> <
-        label >
-        <
-        br / >
-        Email:
-        <
-        br / >
-        <
-        input class = "inputBox"
-        type = "email"
-        name = "replyto" / >
-        <
-        /label> <
-        label >
-        <
-        br / >
-        Suggestions:
-        <
-        br / >
-        <
-        textarea name = "message" > < /textarea> <
-        input id = "sendButton"
-        type = "submit"
-        value = "Send" / >
-        <
-        /label> <
-        /form> <
-        /section> <
-        /div>
+    return ( 
+    <div className = "App3">
+         <header>
+             <div id="myDIV" className="header">
+          <h2>My To Do List</h2>
+          </div>
+        </header>
+        <section>
+        <ul id="myUL">
+          <li><input type="checkbox"/>Hit the gym</li>
+          <li>Pay bills<input type="checkbox"/></li>
+          <li>Meet Charlotte<input type="checkbox"/></li>
+          <li>Buy Groceries<input type="checkbox"/></li>
+          <li>Study<input type="checkbox"/></li>
+          <li>Hit the Sack<input type="checkbox"/></li>
+        </ul>
+        </section>
+        <section>
+          <h1 id="contactHeading">HOW CAN WE DO BETTER?</h1>
+          <form action="https://formspree.io/xqkyryyz" method="POST">
+            <label>
+              <br />
+              Name/Company:
+              <br />
+            <input class="inputBox" type="text" name="name" />
+            </label>
+            <label>
+              <br />
+              Email:
+              <br />
+            <input class="inputBox" type="email" name="replyto"/>
+              </label>
+            <label>
+              <br />
+              Suggestions:
+              <br />
+            <textarea name="message"></textarea>
+            <input id="sendButton" type="submit" value="Send"/>
+            </label>
+          </form>
+        </section>
+        </div>
     );
 }
-export default App3;
+export default App3;*/
